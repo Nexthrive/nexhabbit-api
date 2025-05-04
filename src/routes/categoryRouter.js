c133533678df736bf4804dbe028a58e18cabc546
Edgar <edgarmasstudio@gmail.com>
2025-05-04 12:50:03 +0700

    Update categoryRouter.js

--- src/routes/categoryRouter.js
+++ src/routes/categoryRouter.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { createCategory, getCategoryById, getCategories, } = require("../controllers/categoryController");
+const {
+  createCategory,
+  getCategoryById,
+  getCategories,
+  deleteCategory,
+  updateCategory,
+} = require("../controllers/categoryController");
 const auth = require("../middlewares/auth");
 
 // Apply auth middleware to protect these routes
+router.delete("/deleteCategory/:id", deleteCategory);
+router.patch("/updateCategory/:id", auth, updateCategory); // Apply auth middleware to protect this route
 router.post("/createCategory", createCategory);
 router.get("/getCategories", getCategories);
 router.get("/getCategory/:id", getCategoryById);
 
-
 module.exports = router;
-
